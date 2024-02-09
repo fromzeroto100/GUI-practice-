91405f11bb2565f665bf66177786a4e755929b44
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-09 21:48:23 +0100

    new4

--- task1.py
+++ task1.py
@@ -4,16 +4,18 @@ my_turtle = Turtle()
 
 my_turtle.shape("turtle")
 my_turtle.color("green")
-my_turtle.fd(100)
-my_turtle.right(90)
-my_turtle.fd(100)
-my_turtle.right(90)
-my_turtle.fd(100)
-my_turtle.right(90)
-my_turtle.fd(100)
-my_turtle.right(90)
+# my_turtle.fd(100)
+# my_turtle.right(90)
+# my_turtle.fd(100)
+# my_turtle.right(90)
+# my_turtle.fd(100)
+# my_turtle.right(90)
+# my_turtle.fd(100)
+# my_turtle.right(90)
 
-
+for i in range(4):
+    my_turtle.fd(100)
+    my_turtle.right(90)
 
 
 
