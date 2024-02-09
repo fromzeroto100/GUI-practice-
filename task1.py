f63268c4fe70024786f1c6a95b7749410e71d02e
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-09 21:42:59 +0100

    new2

--- task1.py
+++ task1.py
@@ -2,5 +2,28 @@ from turtle import Turtle, Screen
 
 my_turtle = Turtle()
 
+my_turtle.shape("turtle")
+my_turtle.color("green")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 screen = Screen()
 screen.exitonclick()
