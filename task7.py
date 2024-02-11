12ebc025b5691777fc427f6613cc59aa616f7b31
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-11 11:17:56 +0100

    new2

--- task7.py
+++ task7.py
@@ -3,18 +3,22 @@ import random
 
 t = Turtle()
 t.shape("turtle")
+t.pensize(16)
+t.speed(speed=0)
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", 
           "cyan", "magenta", "pink", "purple", "teal", "lime", "brown", "gray"]
 
-walks = [t.back, t.fd, t.right, t.left]
+# walks = [t.back, t.fd, t.right, t.left]
 
-for walk in walks in range(100):
+# for walk in walks in range(100):
+#     t.color(random.choice(colors))
+#     random.choice(walks)(10)
+directions = [0, 90, 180, 270]
+
+for _ in range(100):
+    t.fd(35)
+    t.setheading(random.choice(directions))
     t.color(random.choice(colors))
-    random.choice(walks)(10)
-
-
-
-
 
 
 screen = Screen()
